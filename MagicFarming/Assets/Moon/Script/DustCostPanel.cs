5a739986af71b3792c29d64477b40b188810c1f7
vivianng1998 <69830300+vivianng1998@users.noreply.github.com>
2021-09-19 12:53:35 +0800

    all icons, pointer enter

---
+++ MagicFarming/Assets/Moon/Script/DustCostPanel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class DustCostPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public enum Action
+    {
+        ATTACK,
+        HEAL,
+        SPEEDUP
+    }
+
+    public Action action;
+    public GameObject panel;
+    public TextMeshProUGUI dustCost;
+
+    private void Start()
+    {
+        panel.SetActive(false);
+
+        switch (action)
+        {
+            case Action.ATTACK:
+                {
+                    dustCost.text = GameManager.instance.attackDustCost.ToString();
+                    break;
+                }
+            case Action.HEAL:
+                {
+                    dustCost.text = GameManager.instance.healDustCost.ToString();
+                    break;
+                }
+            case Action.SPEEDUP:
+                {
+                    dustCost.text = GameManager.instance.speedUpDustCost.ToString();
+                    break;
+                }
+            default:
+                {
+                    dustCost.text = "0";
+                    break;
+                }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        panel.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        panel.SetActive(false);
+    }
+}
